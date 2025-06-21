871c01b04b41f8fe0ed95a411b9056eb592eefc6
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-06-21 17:48:02 +0600

    Auth Layout start

---
+++ src/Layouts/AuthLayout.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import img from '../assets/authImage.png'
+import ProductLogo from '../pages/shared/ProductLogo/ProductLogo';
+
+const AuthLayout = () => {
+    return (
+        <div className="  ">
+            <div>
+                <ProductLogo></ProductLogo>
+            </div>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+               <div className='flex-1'>
+                 <img
+                    src={img}
+                    className="max-w-sm rounded-lg shadow-2xl"
+                />
+               </div>
+                <div className='flex-1'>
+                    <Outlet></Outlet>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AuthLayout;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,8 @@ import {
 } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Authentication/Login/Login";
+import AuthLayout from "../Layouts/AuthLayout";
 
 
 export const router = createBrowserRouter([
@@ -16,4 +18,15 @@ export const router = createBrowserRouter([
       }
     ]
   },
+  {
+    path:"/",
+    Component:AuthLayout,
+    children:[
+     {
+       path:'/login',
+       Component:Login
+     }
+    ]
+
+  }
 ]);
