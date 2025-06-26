9e9011967f3597e7cc02d6581495b84df4e2690d
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-06-26 14:22:14 +0600

    Visa Card

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Element} from '@stripe/react-stripe-js'
+
 import { loadStripe } from '@stripe/stripe-js';
 import PaymentForm from './PaymentForm';
+import { Elements } from '@stripe/react-stripe-js';
 
 const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh')
 
 const Payment = () => {
     return (
-        <Element stripe={stripePromise}>
+        <Elements stripe={stripePromise}>
            <PaymentForm></PaymentForm>
-        </Element>
+        </Elements>
     );
 };
 
--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,32 +1,54 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 const PaymentForm = () => {
 
-    const stripe= useStripe();
+    const stripe = useStripe();
     const element = useElements();
 
-    const handleSubmit = (e) => {
+    const [error,setError] = useState('')
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
 
-        if(!stripe || !element){
+        if (!stripe || !element) {
             return;
         }
 
         const card = element.getElement(CardElement);
 
-        if(!card){
+        if (!card) {
             return;
+        }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if (error) {
+            setError(error.message)
+           
+            // console.log('error', error)
+        }
+        else {
+            setError('')
+            console.log('Payment method', paymentMethod)
         }
     }
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="space-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto">
                 <CardElement>
-                    <button type='submit' disabled={!stripe}>
-                          Pay For Parcel Pickup
-                    </button>
+
                 </CardElement>
+                <button type='submit' disabled={!stripe} className='btn btn-primary text-black'>
+                    Pay For Parcel Pickup
+                </button>
+
+                {
+                    error && <p className='text-red-400'>{error}</p>
+                }
             </form>
         </div>
     );
