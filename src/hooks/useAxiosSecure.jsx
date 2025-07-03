ba8c25b0e584ea70f0b5407b0be789c94866c93d
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-07-03 10:49:11 +0600

    verify admin

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios'
 import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
     baseURL: `http://localhost:3000`
@@ -8,14 +9,40 @@ const axiosSecure = axios.create({
 
 const useAxiosSecure = () => {
 
-    const {user} = useAuth();
+    const { user, logOut } = useAuth();
+    const navigate = useNavigate();
 
     axiosSecure.interceptors.request.use(config => {
-
-     config.headers.Authorization = `Bearer ${user.accessToken}`
+        config.headers.Authorization = `Bearer ${user.accessToken}`
 
         return config;
-    }, error=>{
+    }, error => {
+        return Promise.reject(error)
+    })
+
+    //interceptor
+
+    axiosSecure.interceptors.response.use(res => {
+        return res;
+    }, error => {
+        console.log('inside res interceptor', error.status);
+        const status = error.status;
+
+        if (status === 403) {
+            navigate('/forbidden');
+        }
+
+        else if (status === 401) {
+            logOut()
+                .then(() => {
+                    navigate('/login')
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+
+        }
+
         return Promise.reject(error)
     })
 
